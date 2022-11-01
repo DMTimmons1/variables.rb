a23daff8f48f6bb3f751c35f49574c9ba76af90f
Dawson Timmons <DawsonTimmons@gmail.com>
2022-11-01 15:54:45 -0600

    Reassign variables abd add puts

--- variables.rb
+++ variables.rb
@@ -1,13 +1,70 @@
 email = "dawsontimmons@gmail.com"
-fav_food = "Pasta"
+email # dawsontimmons@gmail.com
+
+email = "randomemail@random.org"
+email # randomemail@random.org
+puts email
+
+fav_food = "pasta"
+fav_food # pasta
+
+fav_food = "pizza"
+fav_food # pizza
+puts fav_food
+
 pet = "murphy"
+pet # murphy
+
+pet = "kevin"
+pet # kevin
+puts pet
+
 
 age = 24
+age # 24
+
+age = 35
+age # 35
+puts age
+
 grad_year = 2017
+grad_year # 2017
+
+grad_year = 2015
+grad_year # 2015
+puts grad_year
+
 pet_num = 1
+pet_num # 1
+
+pet_num = 2
+pet_num # 2 
+puts pet_num
+
 
 balance = 12000.50
-ammount_due = 9999.50
+balance # 12000.50
+
+balance = 10000.10
+balance # 10000.10
+puts balance
+
+amount_due = 9999.50
+amount_due # 9999.50
+
+amount_due = 11999.90
+amount_due # 11999.90
+puts amount_due
+
 
 deposit_paid = false
+deposit_paid # false
+
+deposit_paid = true
+deposit_paid # true
+
 payment_plan = true
+payment_plan # true
+
+payment_plan = false
+payment_plan # false
